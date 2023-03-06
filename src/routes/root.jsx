fbbdd27729a64d36d6de6c81cc76e65b9068973c
V.Mohanapavnkumar <mohanpavankumarvankayala@gmail.com>
2023-03-06 12:30:34 +0530

    Revert "Modifications"
    This reverts commit dd2b39b6bb7e21151777031f7a53e93b85e1cee2.

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -33,7 +33,7 @@ export default function Root() {
 					to={"/departments/IT"}
 				>
 					Departments
-				</Link>
+
 				<Link className=" m-2 p-2 border-2 border-black" to={"/exam-downloads"}>
 					Exam downloads
 				</Link>
