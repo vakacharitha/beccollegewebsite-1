86b1839db6580c0ae35fc2ca593647c34f823998
vakacharitha <110221085+vakacharitha@users.noreply.github.com>
2023-03-06 11:24:42 +0530

    commit2

--- src/Data/SaturatoryData/naacCycles.js
+++ src/Data/SaturatoryData/naacCycles.js
@@ -1,12 +1,12 @@
 export default [
     {
         title: 'Extended Profile',
-        parts: [["1.1","/src/assets/saturatoryData/1.1.1_writeups.pdf"],["1.2","/doc"],["1.3","/doc"]]
+        parts: [["1.1","/doc"],["1.2","/doc"],["1.3","/doc"]]
 
     },
     {
         title: 'Criteria-1',
-        parts: [["1.1","/src/assets/saturatoryData/1.1.2_Syllabus_Revision.pdf"],["1.2","/doc"],["1.3","/doc"]]
+        parts: [["1.1","/doc"],["1.2","/doc"],["1.3","/doc"]]
 
     },
     {
