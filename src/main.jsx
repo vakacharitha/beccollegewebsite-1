a8a960a34a04ec9bea6a560645b1cfc2cd585ab2
shaheensk <110378392+shaheensk@users.noreply.github.com>
2023-03-14 12:03:40 +0530

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -115,7 +115,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<>
 		<React.StrictMode>
-				<RouterProvider router={router} />
+				{/* <RouterProvider router={router} /> */}
 				{/* <BrowserRouter>
       <Sidebar>
         <Routes>
