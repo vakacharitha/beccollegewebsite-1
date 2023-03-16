6bc87ed7bfae7076455e0f2fcb421262cbc283b2
vakacharitha <110221085+vakacharitha@users.noreply.github.com>
2023-03-16 15:29:06 +0530

    Update NaacItems.jsx

--- src/components/saturatoryDataComponents/NaacItems.jsx
+++ src/components/saturatoryDataComponents/NaacItems.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+{/*import { Link } from "react-router-dom";
 import React, {useState} from "react";
 
 
@@ -25,6 +25,35 @@ console.log(title, index, parts)
                     </ul>
                    
                 </div>}
+            </div>
+        </div>
+    )
+}
+export default NaacItem*/}
+import { Link } from "react-router-dom";
+import React, {useState} from "react";
+
+
+function NaacItem({ title, parts, index }) {
+const [item, setItem] = useState()
+    return (
+        <div className="p-4 col px-2 w-300 overflow-x-hidden bg-cyan-100 rounded-xl">
+            <div className='rounded-lg overflow-hidden py-1'>
+            
+               
+                <div className='flex text-xs md:text-sm'>
+                    <ul>
+                    {parts.map((item, i) => (
+                        <>
+                     <h3 className='text-lg md:text-xl mb-2 md:mb-3 font-semibold h-6' onClick={() => setItem(i)}>{title}</h3>
+                     {item == i && <li>
+                        <Link to={item[1]} className='text-lg underline hover:no-underline
+                             text-blue-600 hover:text-blue-800 visited:text-purple-600'>{item[0]}</Link>
+                    </li>}</>)
+                    )}
+                    </ul>
+                   
+                </div>
             </div>
         </div>
     )
